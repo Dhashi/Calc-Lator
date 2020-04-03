f67bb3901992716a5ddb52dda2293dfde568d157
Nicholas <nicholasybiologia@gmail.com>
2020-04-03 18:44:29 -0300

    Reformulação doFuncionamento/Display

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,53 @@
 var screenNumBuffer = ''
+var numContador = 0
+var optContador = 1
 var screenOptBuffer = ''
-var screenNumBuffer2 = ''
+var screenBuffer = ''
+var ar = []
+
+
+// var screenNumBuffer2 = ''
 var result = undefined
 
+//  BOTOES
+function pressNum(number) {
+    screenNumBuffer += number
+
+    ar[numContador] = screenNumBuffer
+
+    console.log(`Number ${numContador}: ${ar[numContador]}`);
+
+    return display()
+
+}
+
+function pressOperator(opt) {
+    screenOptBuffer = opt
+    console.log(`Operador ${optContador}: ${screenOptBuffer}`);
+
+
+    ar[optContador] = screenOptBuffer
+
+    numContador += 2
+    optContador += 2
+    screenNumBuffer = ''
+    screenOptBuffer = ''
+
+    return display()
+}
+
+function display() {
+    const screen = document.getElementById('math')
+
+    ar.forEach(function(nome){
+        screenBuffer += nome
+    })
+
+    screen.innerHTML = `${screenBuffer}`
+    screenBuffer = ''
+}
+
+// RESULTADO
 
 function  calc() {
     let num1 = Number(screenNumBuffer)
@@ -30,61 +75,16 @@ function  calc() {
 }
 
 function displayResu() {
-    const screen = document.getElementById('resu')
+    const screen = document.getElementById('result')
     screen.innerHTML = result
 }
 
-
-function pressNum(number) {
-    if (screenOptBuffer == '') {
-        let num = number
-        let txtNum = number.toString()
-        screenNumBuffer += txtNum
-        console.log(`NUM 1: ${screenNumBuffer}`);
-        return display()
-    }
-}
-
-function display() {
-    const screen = document.getElementById('l1')
-    screen.innerHTML = screenNumBuffer
-}
-
-
-function pressOperator(opt) {
-    screenOptBuffer = opt
-    console.log(screenOptBuffer);
-    return displayOpt()
-}
-
-function displayOpt() {
-    const screen = document.getElementById('l2')
-    screen.innerHTML = screenOptBuffer
-}
-
-
-function pressNum2(number) {
-
-    if (screenNumBuffer != '' && screenOptBuffer != '') {
-        let num = number
-        let txtNum = num.toString()
-        screenNumBuffer2 += txtNum
-        console.log(`NUM 2: ${screenNumBuffer2}`)
-        return display2()
-    }
-}
-
-function display2() {
-    const screen = document.getElementById('l3')
-    screen.innerHTML = screenNumBuffer2
-}
-
+// LIMPADOR
 
 function clean() {
-    const l1 = document.getElementById('l1').innerHTML=""
-    const l2 = document.getElementById('l2').innerHTML=""
-    const l3 = document.getElementById('l3').innerHTML=""
-    const l4 = document.getElementById('resu').innerHTML=""
+    const math = document.getElementById('math').innerHTML=""
+
+    const resu = document.getElementById('result').innerHTML=""
 
     screenNumBuffer = ''
     screenOptBuffer = ''
